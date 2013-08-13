1525656f9412aae0c209f6561b3b1f6d3c26cdc8
TheHalcyonSavant <dennykocev@gmail.com>
2013-08-13 11:55:41 +0200

    First commit

---
+++ ExcelApp/ExcelApp.fs
@@ -0,0 +1,28 @@
+﻿namespace Excel
+
+open Microsoft.Office.Interop.Excel
+
+type App(filePath) =
+    let app = ApplicationClass()
+    let releaseObject(o) =
+        try
+            try System.Runtime.InteropServices.Marshal.ReleaseComObject(o) |> ignore
+            with ex -> printfn "Unable to release the Object %s" (ex.ToString())
+        finally
+            System.GC.Collect()
+    let workbook = app.Workbooks.Open filePath
+    let worksheet = workbook.Worksheets.get_Item(1) :?> _Worksheet
+    member this.xlApp = app
+    member this.xlWorkBook = workbook
+    member this.xlWorkSheet = worksheet
+    member this.Close() =
+        workbook.Close false
+        app.Quit |> ignore
+        releaseObject worksheet
+        releaseObject workbook
+        releaseObject app
+    member this.get (cell:string) = this.xlWorkSheet.get_Range(cell).Value2
+    member this.getByte (cell:string) = this.get(cell) :?> byte
+    member this.getDouble (cell:string) = this.get(cell) :?> double
+    member this.getInt (cell:string) = this.get(cell) :?> int
+    member this.getString (cell:string) = this.get(cell) :?> string
---
+++ ExcelApp/Script.fsx
@@ -0,0 +1,13 @@
+﻿// This file is a script that can be executed with the F# Interactive.  
+// It can be used to explore and test the library project.
+// Note that script files will not be part of the project build.
+#r "office.dll"
+#r "Microsoft.Office.Interop.Excel.dll"
+
+#load "ExcelApp.fs"
+
+open System
+open System.IO
+let a = Excel.App(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),"Excel","recnik.xlsm"))
+printfn "%s" (a.getString("A5"))
+a.Close
